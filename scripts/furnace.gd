eca90e62fa588c5bd5534dc00246c7558bcc24bb
Ben Cerrone <bwc2184@rit.edu>
2024-09-30 16:59:31 -0400

    money display vfx

--- scripts/furnace.gd
+++ scripts/furnace.gd
@@ -1,12 +1,25 @@
 extends Area2D
 class_name Furnace
 @export var item_data: ItemData
+@export var label_scene: PackedScene
+
+func money_vfx(value: float) -> void:
+	var control: Control = label_scene.instantiate()
+	add_child(control)
+	control.get_node("Label").text = "+" + str(value)
+	var pos: Vector2 = Vector2(randf_range(-9, 9), randf_range(-2, -6))
+	control.position += pos
+	var tween: Tween = get_tree().create_tween().set_parallel(true).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(control, "position", control.position + Vector2(0, -10.0), 1.25)
+	tween.tween_property(control, "modulate", Color.TRANSPARENT, 1.25)
+	tween.finished.connect(control.queue_free)
 
 func process_item(ore: Ore) -> void:
 	var ore_value: float = ore.value
 	$AudioStreamPlayer2D.play()
 	$CPUParticles2D.emitting = true
 	Player.add_money(ore_value)
+	money_vfx(ore_value)
 	ore.queue_free()
 
 func can_recieve_item():
